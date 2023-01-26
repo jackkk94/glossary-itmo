94a24f069ea4f2f8e97d9d8eb8a2e2d6f0987824
jackkk94 <e.panchenko@socialquantum.com>
2023-01-27 00:49:27 +0300

    fix

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,17 @@ const loadFile = (file, callback) => {
 const main = async () => {
   let list = [];
 
-  // loadFile('./list.json', (data, err) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return;
-  //   }
+  loadFile('./list.json', (data, err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-  //   list = data ?? [];
-  // });
+    list = data ?? [];
+  });
 
   app.get('/list', async (request, response) => {
-    response.send([{id:1, title: '1', description:'1212'}]);
+    response.send(list);
   });
 
   app.get('/list/:id', async (request, response) => {
